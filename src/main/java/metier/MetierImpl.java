15b9f9ce27582d9e5203be49e4659232cfb03497
Imane YAHYA <imaneyahya86@gmail.com>
2024-02-19 10:23:04 +0100

    TP1: Inversion de contrôle et Injection des dépendances

---
+++ src/main/java/metier/MetierImpl.java
@@ -0,0 +1,26 @@
+package metier;
+
+import dao.IDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("metier")
+public class MetierImpl implements IMetier {
+    @Autowired
+    private IDao dao;//Couplage faible
+
+    public MetierImpl(IDao dao) {
+        this.dao = dao;
+    }
+
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public double calcul() {
+        double tmp=dao.getData();
+        double res = tmp*600/Math.cos(tmp*Math.PI);
+        return res;
+    }
+}
---
+++ src/main/java/pres/Presentation.java
@@ -0,0 +1,14 @@
+package pres;
+
+import ext.DaoImpl2;
+import metier.MetierImpl;
+
+public class Presentation {
+    public static void main(String[] args) {
+        // injection des dependances par instantiation statique
+      DaoImpl2 dao = new DaoImpl2();
+        MetierImpl metier = new MetierImpl(dao);
+        //metier.setDao(dao);
+        System.out.println("Resultat =" +metier.calcul());
+    }
+}
